47037f1c477fe1745c08094c93f930eb40c10c3a
Edward Patch <crazygaming055@gmail.com>
2023-06-26 15:33:17 +0100

    Updated tests and implemented. Test Status: Passed
    Update

--- tests/component_tests/preparation/test_convert_to_model.py
+++ tests/component_tests/preparation/test_convert_to_model.py
@@ -1,33 +1,40 @@
 import unittest
 import pandas as pd
-import numpy as np
-import tensorflow as tf
 import os
+import shutil
 
 from modules.pre_processing.CSV_PreProcessor import (
-    combine_csv,
     load_csv_from_path,
-    load_and_preprocess_image,
-    load_and_preprocess_from_path_label,
+    combine_csv
+)
+
+from modules.pre_processing.CSV_Conversion import (
+    csv_to_model_format,
+    yolo_to_rcnn_bbox
 )
 
 class TestConversionDataPreparation(unittest.TestCase):
-    prefix_dir = "./tests/component_tests/preparation/csv/mapping/"
-    output_dir = os.path.join(prefix_dir, "output.csv")
+    prefix_dir: str = "./tests/component_tests/preparation"
+    prefix_dir_csv: str = f'{prefix_dir}/csv/mapping/'
+    prefix_dir_output: str = f'{prefix_dir}/model_conversion/'
+
+    output_dir_csv: str = os.path.join(prefix_dir_csv, "output.csv")
+    output_dir_yolo: str = os.path.join(prefix_dir_output, "output.yolo")
+    output_dir_rcnn: str = os.path.join(prefix_dir_output, "output.rcnn")
 
     def setUp(self) -> None:
         self.df = pd.DataFrame()
 
         output_file: list[str] = [
-            os.path.join(self.prefix_dir, "test1.csv"),
-            os.path.join(self.prefix_dir, "test2.csv"),
+            os.path.join(self.prefix_dir_csv, "test1.csv"),
+            os.path.join(self.prefix_dir_csv, "test2.csv"),
         ]
 
-        combine_csv(output_file, self.output_dir)
+        combine_csv(output_file, self.output_dir_csv)
 
-        self.assertTrue(os.path.exists(self.output_dir))
+        self.assertTrue(os.path.exists(self.output_dir_csv))
 
-        train_df, test_df = load_csv_from_path(self.output_dir)
+        train_df, test_df = load_csv_from_path(self.output_dir_csv)
 
         self.assertIsInstance(train_df, pd.DataFrame)
         self.assertIsInstance(test_df, pd.DataFrame)
@@ -35,22 +42,29 @@ class TestConversionDataPreparation(unittest.TestCase):
         self.xY = (train_df, test_df)
 
     # Test 1: Failed (Not Implemented)
-    def test_csv_to_(self) :
-        self.assertIsNone(True)
-        
-    # Test 1: Failed (Not Implemented)
-    def test_csv_to_rcnn(self) :
-        self.assertIsNone(True)
+    # Test 2: Passed (Implemented)
+    def test_csv_to_yolo(self) :
+        csv_to_model_format(self.output_dir_csv, self.output_dir_yolo)
 
     # Test 1: Failed (Not Implemented)
+    # Test 2: Passed (Implemented)
     def test_yolo_to_rcnn_bbox(self) :
-        self.assertIsNone(True)
+        result = yolo_to_rcnn_bbox(['0.5', '0.5', '0.2', '0.2'])
+        self.assertListEqual(result, [0.4, 0.4, 0.6, 0.6])
+        
+    # Test 1: Failed (Not Implemented)
+    # Test 2: Passed (Implemented)
+    def test_csv_to_rcnn(self) :
+        csv_to_model_format(self.output_dir_csv, self.output_dir_rcnn, 'RCNN')
 
     @classmethod
     def tearDownClass(cls) -> None:
-        if os.path.exists(cls.output_dir):
-            os.remove(cls.output_dir)
-
-
+        if os.path.isdir(cls.output_dir_yolo):
+                shutil.rmtree(cls.output_dir_yolo)
+        if os.path.isdir(cls.output_dir_rcnn):
+            shutil.rmtree(cls.output_dir_rcnn)
+        if os.path.exists(cls.output_dir_csv):
+            os.remove(cls.output_dir_csv)
+        
 if __name__ == "__main__":
     unittest.main()
