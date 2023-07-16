25c2ada40bc1b7681c55febd367d0b38e38a6cd9
Edward Patch <crazygaming055@gmail.com>
2023-07-16 12:54:59 +0100

    Update Video to Image

--- workbench/modules/video_processing/video_to_image.py
+++ workbench/modules/video_processing/video_to_image.py
@@ -2,14 +2,14 @@ import cv2
 import os
 
 
-def video_to_frames(video_path: str, output_dir: str) -> None:
+def video_to_frames(video_path: str, output_dir: str, sample_name: str) -> None:
     vidcap = cv2.VideoCapture(video_path)
     success, image = vidcap.read()
     count: int = 0
     while success:
         cv2.imwrite(os.path.join(output_dir, f"frame{count}.jpg"), image)
         success, image = vidcap.read()
-        print(f"Saved frame{count}.jpg")
+        print(f"{sample_name}-{count}.jpg")
         count += 1
 
     print("Done extracting frames.")
